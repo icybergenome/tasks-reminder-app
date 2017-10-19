8d6a4207f5f95014bdadcec094b7d211b46263a3
Bilal <bilalsaeedhb@gmail.com>
2017-10-19 23:58:53 +0500

    Basic App layout

---
+++ src/components/App.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <div className="title">
+          Reminder Pro
+        </div>
+        <div className="form-inline">
+          <div className="form-group">
+            <input
+              className="form-control"
+              placeholder="I have to ...."
+            />
+          <button
+            type="button"
+            className="btn btn-success"
+          >
+            Add Reminder
+          </button>
+          </div>
+        </div>
+
+      </div>
+
+    )
+  }
+}
+
+export default App;
